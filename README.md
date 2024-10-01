79e533adbb44810e1439095f789e80318fd530f9
Erik Johnson <airik.k.johnson@gmail.com>
2024-10-01 13:55:56 -0600

    fixed bug in division to throw an error if denominator is 0

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # hw-1-scripting-and-version-control
+
+## Changelog
+
+### v1.0.0
+
+- Fixed bug in `division` function.
+`division` now throws an error if the denominator is 0.
--- math_lib.py
+++ math_lib.py
@@ -1,8 +1,4 @@
 def division(a, b):
     if b == 0:
-        return 0
+        raise ValueError("Denominator should be non-zero")
     return a / b
-
-
-def add(a, b):
-    return a + b
